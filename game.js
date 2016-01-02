c2a859f538b3278d0dfa51aa2a827c44e75cf510
Michael Sullivan <sully@msully.net>
2016-01-01 23:54:26 -0600

    Get it working a bit better

--- game.js
+++ game.js
@@ -1,36 +1,72 @@
+var tileData = {
+    bareFloor: {set: 'tiles1', x: 3, y: 28},
+    mageFront: {set: 'mage_f', x: 0, y: 2, height: 36},
+    mage2Front: {set: 'chars', x: 0, y: 4},
+}
+
+
+// Some data
 
 var TILE = 32;
-var X_TILES = 40;
-var Y_TILES = 40;
+var X_TILES = 20;
+var Y_TILES = 20;
 
 // Lol.
 var game = {};
 
+
+/////////////////////////////////////////////
+
 // This seems like a really dumb way to do this.
-function getTile(set, xtile, ytile) {
-    var canvas=document.createElement('canvas')
-    var context=canvas.getContext('2d');
-    var imgElement = document.getElementById(set);
+function loadTile(spec, cb=null) {
+    var canvas = document.createElement('canvas');
+    canvas.height = canvas.width = TILE;
+    var context = canvas.getContext('2d');
+    var imgElement = document.getElementById(spec.set);
+
+    var width = spec.width || TILE;
+    var height = spec.height || TILE;
     context.drawImage(imgElement,
-                      xtile*TILE, ytile*TILE, TILE, TILE,
+                      spec.x*width, spec.y*height, width, height,
                       0, 0, TILE, TILE);
 
-    var image = new Image();
-    image.src = canvas.toDataURL();
-    // XXX: Delete or something?
+    var dataurl = canvas.toDataURL('image/png');
+    //var image = new Image();
+    var image = fabric.util.createImage();
+    image.onload = cb;
+    image.src = dataurl;
+    // does this leak a canvas in a bad way or something?
     return image;
 }
 
-function draw() {
+function loadTiles(specs, cb) {
+    var tiles = {};
+    var total = 0;
+    var count = 0;
+    function innercb() {
+        if (++count == total) {
+            cb(tiles);
+        }
+    }
+
+    for (var tile in specs) {
+        total++;
+        var spec = specs[tile];
+        tiles[tile] = loadTile(spec, innercb);
+    }
+}
+
+///////////////////////////////////////////////
+
+function draw(tiles) {
     // create a wrapper around native canvas element (with id="c")
-    var canvas = new fabric.StaticCanvas('game');
+    var canvas = this.__canvas = new fabric.StaticCanvas('game');
 
     // Draw floor
-    var floor = getTile('tiles1', 3, 28);
     for (var x = 0; x < X_TILES; x++) {
         for (var y = 0; y < Y_TILES; y++) {
-            console.log("doing " + x + "," + y);
-            var imgInstance = new fabric.Image(floor, {
+            //console.log("doing " + x + "," + y);
+            var imgInstance = new fabric.Image(tiles.bareFloor, {
                 left: x*TILE,
                 top: y*TILE,
             });
@@ -38,20 +74,16 @@ function draw() {
         }
     }
 
-    // create a rectangle object
-    var rect = new fabric.Rect({
-        left: 100,
-        top: 100,
-        fill: 'red',
-        width: 20,
-        height: 20
+    var noob = new fabric.Image(tiles.mage2Front, {
+        left: 5*TILE,
+        top: 2*TILE,
     });
+    canvas.add(noob);
 
-    // "add" rectangle onto canvas
-    canvas.add(rect);
+    canvas.renderAll();
 }
 
 
 function init() {
-    draw();
+    loadTiles(tileData, draw);
 }
